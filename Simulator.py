4f4a7d97ed5e903b85b81e0c7958e0c781c6822b
Skbhudolia <92352472+Skbhudolia@users.noreply.github.com>
2024-04-17 17:13:50 +0530

    Update Simulator.py

--- Simulator.py
+++ Simulator.py
@@ -1,180 +1,207 @@
+import sys
+
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+a = open(output_file, "w")
+a.close()
+
 class RISC_V_Simulator:
     def __init__(self):
         self.registers = [0] * 32
-        self.memory = [0] * 4096  # 4KB memory
-        self.pc = 0  # Program Counter
-        self.last_line = 0
+        self.memory = [0] * 32
+        self.pc = 0
 
-    def execute(self, program):
-        while self.pc <= len(program) * 4:
-            instruction = program[self.pc // 4]
-            opcode=instruction[-1:-7:-1]
-            print(opcode)
-            opcode, *args = instruction.split()
-            if opcode in ["add", "sub", "sll", "slt", "sltu", "xor", "srl", "sra", "or", "and"]:
-                self.r_type(instruction)
-            elif opcode in ["lw", "sw", "jalr", "addi", "slti", "sltiu", "xori", "ori", "andi", "slli", "srli", "srai"]:
-                self.u_type(instruction)
-            elif opcode in ["sw"]:
-                self.s_type(instruction)
-            else:
-                print("Unknown instruction:", opcode)
-                break
+    def int_to_bin(self, num, width=32):
+        width = width - 1
+        if num < 0:
+            num = (2 ** (width)) + num
+            output = "0b1" + format(num, f'0{width}b')
+            return output[2:]
+        output = "0b0" + format(num, f'0{width}b')
+        return output[2:]
 
-    def imm_or_label(self, given):
-        pass  # Implement this function
+    def sext(self, bin, width):
+        nw = width - len(bin)
+        return (bin[0] * nw) + bin
 
-    def sign_extend(self, value, bits):
-        if value >> (bits - 1) & 1:  # Check if the sign bit is set
-            return value | ((1 << (32 - bits)) - 1) << bits
-        return value
+    def bin_to_int(self, bin, signed):
+        if signed == 1:
+            if bin[0] == '1':
+                width = len(bin) - 1
+                num = (2 ** width) - int(bin[1:], 2)
+                return -num
+            return int(bin[1:], 2)
+        elif signed == 0:
+            return int(bin, 2)
 
+    def register_output(self):
+        output = self.int_to_bin(self.pc, 32)
+        for i in self.registers:
+            output = output + " " + self.int_to_bin(i, 32)
+        output = output + "\n"
+        with open(output_file, "a") as file:
+            file.write(output)
 
-    def twos_compliment(self, no):
-        pass  # Implement this function
+    def memory_output(self):
+        output = ""
+        for i, j in enumerate(self.memory):
+            output = output + "0x" + format(4 * i + 65536, "08x") + ":" + self.int_to_bin(j, 32) + "\n"
+        with open(output_file, "a") as file:
+            file.write(output)
 
-    def bin_converter(self, no, width=32):
-        pass  # Implement this function
+    def rtype(self, ins):
+        rd = ins[::-1][7:12][::-1]
+        rs1 = ins[::-1][15:20][::-1]
+        rs2 = ins[::-1][20:25][::-1]
+        funct3 = ins[::-1][12:15][::-1]
+        funct7 = ins[::-1][25:32][::-1]
 
-    def j_type(self, instruction):
-        pass  # Implement this function
+        if funct3 == '000' and funct7 == '0000000':  # ADD
+            self.registers[self.bin_to_int(rd, 0)] = self.registers[self.bin_to_int(rs1, 0)] + self.registers[self.bin_to_int(rs2, 0)]
+        elif funct3 == '000' and funct7 == '0100000':  # SUB
+            self.registers[self.bin_to_int(rd, 0)] = self.registers[self.bin_to_int(rs1, 0)] - self.registers[self.bin_to_int(rs2, 0)]
+        elif funct3 == '111' and funct7 == '0000000':  # AND
+            self.registers[self.bin_to_int(rd, 0)] = self.registers[self.bin_to_int(rs1, 0)] & self.registers[self.bin_to_int(rs2, 0)]
+        elif funct3 == '110' and funct7 == '0000000':  # OR
+            self.registers[self.bin_to_int(rd, 0)] = self.registers[self.bin_to_int(rs1, 0)] | self.registers[self.bin_to_int(rs2, 0)]
+        elif funct3 == '100' and funct7 == '0000000':  # XOR
+            self.registers[self.bin_to_int(rd, 0)] = self.registers[self.bin_to_int(rs1, 0)] ^ self.registers[self.bin_to_int(rs2, 0)]
+        elif funct3 == '001' and funct7 == '0000000':  # SLL
+            elf.registers[self.bin_to_int(rd, 0)] = self.registers[self.bin_to_int(rs1, 0)] << self.bin_to_int(self.int_to_bin(self.registers[self.bin_to_int(rs2, 0)] )[2:][-5:], 0)
+        elif funct3 == '010' and funct7 == '0000000':  # SLT
+            self.registers[self.bin_to_int(rd, 0)] = 1 if self.registers[self.bin_to_int(rs1, 0)] < self.registers[self.bin_to_int(rs2, 0)] else 0
+        elif funct3 == '011' and funct7 == '0000000':  # SLTU
+            self.registers[self.bin_to_int(rd, 0)] = 1 if self.bin_to_int(self.int_to_bin(self.registers[self.bin_to_int(rs1, 0)])[2:], 0) < self.bin_to_int(self.int_to_bin(self.registers[self.bin_to_int(rs2, 0)])[2:], 0) else 0
+        elif funct3 == '101' and funct7 == '0000000':  # SRL
+            self.registers[self.bin_to_int(rd, 0)] = self.registers[self.bin_to_int(rs1, 0)] >> self.bin_to_int(self.int_to_bin(self.registers[self.bin_to_int(rs2, 0)] )[2:][-5:], 0)
 
-    def u_type(self, instruction):
-        pass  # Implement this function
-
-    def s_type(self, instruction):
-        opcode, offset_rs2_rs1 = instruction.split()
-        offset, rs2_rs1 = offset_rs2_rs1.split("(")
-        offset = int(offset)
-        rs2, rs1 = rs2_rs1[:-1].split(",")
-        rs1 = int(rs1[1:])  # Extract base register number
-        rs2 = int(rs2[1:])  # Extract source register number
-    
-        address = self.registers[rs1] + offset
-        value = self.registers[rs2]
-    
-        # Perform sign extension if necessary
-        offset = self.sign_extend(offset, 12)
-    
-        # Ensure address is aligned if storing a word
-        if address % 4 != 0:
-            print("Unaligned memory access for sw instruction.")
-            return
-        else:
-            self.memory[address] = value
-
-    def r_type(self, instruction):
-        opcode, rd, rs1, rs2 = instruction.split()
-        rd = int(rd[1:])  # Extract destination register number
-        rs1 = int(rs1[1:])  # Extract source register 1 number
-        rs2 = int(rs2[1:])  # Extract source register 2 number
-
-        def sign_extend(value, bits):
-            if value >> (bits - 1) & 1:  # Check if the sign bit is set
-                return value | ((1 << (32 - bits)) - 1) << bits
-            return value
-
-        if opcode == "slt":
-            rs1_value = self.registers[rs1]
-            rs2_value = self.registers[rs2]
-            rs1_value = sign_extend(rs1_value, 32)
-            rs2_value = sign_extend(rs2_value, 32)
-            self.registers[rd] = 1 if rs1_value < rs2_value else 0
-        elif opcode == "add":
-            rs1_value = self.registers[rs1]
-            rs2_value = self.registers[rs2]
-            rs1_value = sign_extend(rs1_value, 32)
-            rs2_value = sign_extend(rs2_value, 32)
-            self.registers[rd] = rs1_value + rs2_value
-        elif opcode == "sub":
-            rs1_value = self.registers[rs1]
-            rs2_value = self.registers[rs2]
-            rs1_value = sign_extend(rs1_value, 32)
-            rs2_value = sign_extend(rs2_value, 32)
-            self.registers[rd] = rs1_value - rs2_value
-        elif opcode == "sll":
-            self.registers[rd] = self.registers[rs1] << (self.registers[rs2] & 0x1F)
-        elif opcode == "sltu":
-            self.registers[rd] = 1 if self.registers[rs1] < self.registers[rs2] else 0  # Unsigned comparison
-        elif opcode == "xor":
-            self.registers[rd] = self.registers[rs1] ^ self.registers[rs2]
-        elif opcode == "srl":
-            self.registers[rd] = self.registers[rs1] >> (self.registers[rs2] & 0x1F)  # Logical right shift
-        elif opcode == "or":
-            self.registers[rd] = self.registers[rs1] | self.registers[rs2]
-        elif opcode == "and":
-            self.registers[rd] = self.registers[rs1] & self.registers[rs2]
-
-    def b_type(self, instruction): #fix Imm later
-        opcode, rs1, rs2, imm = instruction.split()
-        rs1 = int(rs1[1:])
-        rs2 = int(rs2[1:])
-        imm = int(imm)
-
-        def sign_extend(value, bits):
-            if value >> (bits - 1) & 1:  # Check if the sign bit is set
-                return value | ((1 << (32 - bits)) - 1) << bits
-            return value
-        
-        def signed_to_unsigned(value):
-            if value < 0:
-                return value & 0xFFFFFFFF
-            else:
-                return value
-            
-        if opcode == "beq":
-            rs1_value = self.registers[rs1]
-            rs2_value = self.registers[rs2]
-            rs1_value = sign_extend(rs1_value,32)
-            rs2_value = sign_extend(rs2_value,32)
-            self.pc = self.pc+imm if rs1_value == rs2_value else 0
-
-        if opcode == "bne":
-            rs1_value = self.registers[rs1]
-            rs2_value = self.registers[rs2]
-            rs1_value = sign_extend(rs1_value,32)
-            rs2_value = sign_extend(rs2_value,32)
-            self.pc = self.pc+imm if rs1_value != rs2_value else 0
-
-        if opcode == "bge":
-            rs1_value = self.registers[rs1]
-            rs2_value = self.registers[rs2]
-            rs1_value = sign_extend(rs1_value,32)
-            rs2_value = sign_extend(rs2_value,32)
-            self.pc = self.pc+imm if rs1_value >= rs2_value else 0
-
-        if opcode == "bgeu":
-            rs1_value = self.registers[rs1]
-            rs2_value = self.registers[rs2]
-            rs1_value = sign_extend(signed_to_unsigned(rs1_value))
-            rs2_value = sign_extend(signed_to_unsigned(rs2_value))
-            self.pc = self.pc+imm if rs1_value >= rs2_value else 0
-
-        if opcode == "bltu":
-            rs1_value = self.registers[rs1]
-            rs2_value = self.registers[rs2]
-            rs1_value = sign_extend(signed_to_unsigned(rs1_value))
-            rs2_value = sign_extend(signed_to_unsigned(rs2_value))
-            self.pc = self.pc+imm if rs1_value < rs2_value else 0
-
-        if opcode == "blt":
-            rs1_value = self.registers[rs1]
-            rs2_value = self.registers[rs2]
-            rs1_value = signed_to_unsigned(rs1_value)
-            rs2_value = signed_to_unsigned(rs2_value)
-            self.pc = self.pc+imm if rs1_value < rs2_value else 0
-
+        self.pc += 1
 
     def i_type(self, instruction):
-        pass  # Implement this function
+        opcode = instruction[-7:]
+        imm = instruction[::-1][20:32][::-1]
+        imm_value = self.bin_to_int(imm, 1)
+        rd = self.bin_to_int(instruction[::-1][7:12][::-1], 0)
+        rs1 = self.bin_to_int(instruction[::-1][15:20][::-1], 0)
 
-    
+        funct3 = instruction[::-1][12:15][::-1]
 
+        if opcode == '1100111':  # jalr
+            self.registers[rd] = 4 * self.pc + 4
+            self.pc = (self.registers[rs1] + imm_value) // 4
 
-# Example usage:
-if __name__ == "__main__":
-    simulator = RISC_V_Simulator()
-    with open("input.txt", "r") as f:
-        program = f.readlines()
+        elif opcode == '0010011':  # sltiu
+            if funct3 == '011':
+                self.registers[self.bin_to_int(rd, 0)] = 1 if self.bin_to_int(self.int_to_bin(self.registers[self.bin_to_int(rs1, 0)])[2:], 0) < self.bin_to_int(imm, 0) else 0
+            elif funct3 == '000':
+                self.registers[self.bin_to_int(rd, 0)] = self.registers[self.bin_to_int(rs1, 0)] + imm_value
 
-    simulator.execute(program)
+        elif opcode == "0000011":
+            if funct3 == '010':
+                self.registers[self.bin_to_int(rd, 0)] = self.memory[(self.registers[self.bin_to_int(rs1, 0)] + imm_value - 16 ** 4) // 4]
+
+        self.pc += 1
+
+    def stype(self, ins):
+        imm = ins[::-1][25:32][::-1] + ins[::-1][7:12][::-1]
+        immval = self.bin_to_int(imm, 1)
+        rs1 = ins[::-1][15:20][::-1]
+        rs2 = ins[::-1][20:25][::-1]
+        funct3 = ins[::-1][12:15][::-1]
+        memadr = immval + self.registers[self.bin_to_int(rs1, 0)] - 16 ** 4
+        self.memory[memadr // 4] = self.registers[self.bin_to_int(rs2, 0)]
+        self.pc += 1
+
+    def btype(self, instruction):
+        opcode = instruction[-7:]
+        imm = instruction[-32] + instruction[-8] + instruction[-31:-25] + instruction[-12:-7] + '0'
+        rs1 = self.bin_to_int(instruction[::-1][15:20][::-1], 0)
+        rs2 = self.bin_to_int(instruction[::-1][20:25][::-1], 0)
+        funct3 = instruction[::-1][12:15][::-1]
+
+        imm_value = self.bin_to_int(imm, 1)
+        rs1_value = self.registers[rs1]
+        rs2_value = self.registers[rs2]
+
+        done = False
+
+        if funct3 == '000':  # beq
+            if rs1_value == rs2_value:
+                self.pc = self.pc + imm_value // 4
+                done = True
+        elif funct3 == '001':  # bne
+            if rs1_value != rs2_value:
+                self.pc = self.pc + imm_value // 4
+                done = True
+        elif funct3 == '101':  # bge
+            if rs1_value >= rs2_value:
+                self.pc = self.pc + imm_value // 4
+                done = True
+        elif funct3 == '111':  # bgeu
+            if rs1_value >= rs2_value:
+                self.pc = self.pc + imm_value // 4
+                done = True
+        elif funct3 == '110':  # bltu
+            if rs1_value < rs2_value:
+                self.pc = self.pc + imm_value // 4
+                done = True
+        elif funct3 == '100':  # blt
+            if rs1_value < rs2_value:
+                self.pc = self.pc + imm_value // 4
+                done = True
+
+        if not done:
+            self.pc += 1
+
+    def utype(self, ins):
+        opcode = ins[-7:]
+        rd = ins[::-1][7:12][::-1]
+        rdint = self.bin_to_int(rd, 0)
+        imm = ins[::-1][12:32][::-1]
+        if opcode == "0110111":  # lui
+            self.registers[rdint] = self.bin_to_int(imm + "0" * 12, 1)
+        elif opcode == "0010111":  # auipc
+            self.registers[rdint] = self.bin_to_int(imm + "0" * 12, 1) + 4 * self.pc
+
+        self.pc += 1
+
+    def j_type(self, instruction):
+        rd = instruction[-12:-7]
+        imm = "0" * 21
+        imm[-21] = instruction[-32]
+        imm[-11:-1] = instruction[-31:-21]
+        imm[-12] = instruction[-21]
+        imm[-20:-12] = instruction[-20:-12]
+
+        self.registers[self.bin_to_int(rd, 0)] = 4 * self.pc + 4
+        self.pc += self.bin_to_int(self.sext(imm, 32)) // 4
+        self.pc += 1
+
+    def execute(self, input_file):
+        read = open(input_file, 'r')
+        program = [line.strip() for line in read.readlines() if line]
+        while self.pc < len(program):
+            opcode = program[self.pc][-7:]
+
+            if opcode == '0110011':  # r_type
+                self.rtype(program[self.pc])
+            elif opcode in ['0000011', '0010011', '1100111']:  # i_type
+                self.i_type(program[self.pc])
+            elif opcode == '0100011':  # s_type
+                self.stype(program[self.pc])
+            elif opcode == '1100011':  # b_type
+                self.btype(program[self.pc])
+            elif opcode in ['0110111', '0010111']:
+                self.utype(program[self.pc])
+            elif opcode == '1101111':
+                self.j_type(program[self.pc])
+            else:
+                print(f"Unknown opcode: {opcode}")
+                break
+
+            self.register_output()
+
+        self.memory_output()
+
+sim = RISC_V_Simulator()
+sim.execute(input_file)
